36e6233e1e424fc717352fe42db5f0a814ae2900
buddaguitarz <40493406+buddaguitarz@users.noreply.github.com>
2026-01-09 10:58:32 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ pd_df=my_dataframe.to_pandas()
 ingredients_List = st.multiselect(
     'Choose up to 5 ingrediants:'
     #, my_dataframe    
-    pd_df['FRUIT_NAME'].tolist(),
+    ,pd_df['FRUIT_NAME'].tolist(),
     , max_selections=5
     )
 
